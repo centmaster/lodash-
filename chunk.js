d853491b5d0c38de95ad5d9f3f436eb0747e8a09
centmaster <861676131@qq.com>
2018-02-09 16:43:47 +0800

    first commit

---
+++ chunk.js
@@ -0,0 +1,16 @@
+/** 
+* @param {Array} array The array to process.
+* @param {number} [size=1] The length of each chunk
+* @returns {Array} Returns the new array of chunks.
+* @example
+*
+* chunk(['a', 'b', 'c', 'd'], 2)
+* // => [['a', 'b'], ['c', 'd']]
+*
+* chunk(['a', 'b', 'c', 'd'], 3)
+* // => [['a', 'b', 'c'], ['d']]
+*/
+
+function chunk(){
+    
+}
